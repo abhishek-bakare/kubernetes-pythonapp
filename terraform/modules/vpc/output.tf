373d381aec0348112eaa842fdabdb1befaf52c2a
Abhishek Bakare <abhishekbakare43398@gmail.com>
2025-04-19 12:45:40 +0530

    changes in vpc module output

--- terraform/modules/vpc/output.tf
+++ terraform/modules/vpc/output.tf
@@ -4,3 +4,27 @@ output "vpc_id" {
     description = "The ID of the VPC"
   
 }
+
+output "subnet_id1" {
+
+        
+    value = aws_subnet.eks_vpc_public_subnet1.id
+    description = "The ID of the first public subnet"
+  
+}
+
+output "subnet_id2" {
+
+        
+    value = aws_subnet.eks_vpc_public_subnet2.id
+    description = "The ID of the second public subnet"
+  
+}
+
+output "subnet_id3" {
+
+        
+    value = aws_subnet.eks_vpc_public_subnet3.id
+    description = "The ID of the third public subnet"
+  
+}
