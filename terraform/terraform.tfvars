6e6e3fe982b5a6d16e9cacda9d09714df21ac783
Abhishek Bakare <abhishekbakare43398@gmail.com>
2025-04-20 12:45:37 +0530

    created tfvars for better code

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,5 @@
+cluster_name = "my-eks"
+endpoint_public_access = true
+endpoint_private_access = false
+cluster_role = "eksClusterRole"
+node_role = "eksNodeRole"
--- terraform/variable.tf
+++ terraform/variable.tf
@@ -2,7 +2,6 @@ variable "cluster_name" {
 
     description = "The name of the cluster"
     type        = string
-    default     = "my-eks"
   
 }
 
@@ -10,7 +9,6 @@ variable "endpoint_public_access" {
 
     description = "Whether to enable private access to the cluster endpoint"
     type        = bool
-    default     = true
   
 }
 
@@ -18,7 +16,6 @@ variable "endpoint_private_access" {
     
     description = "Whether to enable private access to the cluster endpoint"
     type        = bool
-    default     = false
   
 }
 
@@ -26,7 +23,6 @@ variable "cluster_role" {
 
     description = "The name of the cluster role"
     type        = string
-    default     = "eksClusterRole"
   
 }
 
@@ -34,6 +30,5 @@ variable "node_role" {
 
     description = "The name of the node role"
     type = string
-    default = "eksNodeRole"
   
 }
